df342bd20a1cc75a09021733750788db14f1cb1e
NRotondaro <niksonrotondaro1@gmail.com>
2022-11-14 09:24:28 +0000

    feat(cypress): hooks

--- cypress/integration/examples/class.spec.js
+++ cypress/integration/examples/class.spec.js
@@ -19,8 +19,36 @@ class HomePage extends BasePage {
 }
 
 describe('Abstraction with classes', () => {
-	it('should scroll down and up on the page', () => {
+	before(function () {
+		// reuns before all tests inside describe
+		// setup test data or test context
+		// seed or reset the database
 		HomePage.loadHomePage();
+	});
+
+	after(function () {
+		// runs after all tests inside describe block are done
+		// test clean up
+		// clean cookies or localStorage
+	});
+
+	beforeEach(function () {
+		// runs before each it block in the describe
+	});
+
+	afterEach(function () {
+		// runs after each it block in the describe
+	});
+
+	it.skip('1st IT', () => {
+		HomePage.scrollToBottom();
+		HomePage.wait(5000);
+		HomePage.scrollToTop();
+		HomePage.wait(3000);
+		HomePage.scrollToBottom();
+	});
+
+	it.only('2nd IT', () => {
 		HomePage.scrollToBottom();
 		HomePage.wait(5000);
 		HomePage.scrollToTop();
