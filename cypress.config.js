a0317c996e22988be834da5b1c69480e409ff0cf
NRotondaro <niksonrotondaro1@gmail.com>
2022-11-14 12:09:19 +0000

    feat(cypress): full login workflow test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  }
-})
+	/* watchForFileChanges: true, */
+	viewportWidth: 1000,
+	viewportHeight: 600,
+	waitForAnimations: true,
+	animationDistanceThreshold: 20,
+	defaultCommandTimeout: 5000,
+	execTimeout: 60000,
+	pageLoadTimeout: 60000,
+	requestTimeout: 15000,
+	responseTimeout: 15000,
+	video: false,
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+});
