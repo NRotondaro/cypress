dc149b4d3e1595dbd7feb0bb3066351ab7764666
NRotondaro <niksonrotondaro1@gmail.com>
2022-11-14 14:33:53 +0000

    feat(cypress): payment tests with commands and hooks

--- cypress/integration/e2e_tests/login.spec.js
+++ cypress/integration/e2e_tests/login.spec.js
@@ -6,10 +6,7 @@ describe('Login / Logout Test', () => {
 	});
 
 	it('should try to login with invalid data', () => {
-		cy.get('#login_form').should('be.visible');
-		cy.get('#user_login').type('invalid username');
-		cy.get('#user_password').type('invalid password');
-		cy.contains('Sign in').click();
+		cy.login('invalid username', 'invalid password');
 	});
 
 	it('should display error message', () => {
@@ -23,10 +20,7 @@ describe('Login / Logout Test', () => {
 			const username = user.id;
 			const password = user.pwd;
 
-			cy.get('#user_login').type(username);
-			cy.get('#user_password').type(password);
-			cy.get('#user_remember_me').click();
-			cy.contains('Sign in').click();
+			cy.login(username, password);
 		});
 		cy.get('ul.nav-tabs').should('be.visible');
 	});
