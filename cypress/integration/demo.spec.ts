d49f8d2b984ca045db702675fdb6cee9d103a6bc
NRotondaro <niksonrotondaro1@gmail.com>
2022-11-14 17:21:51 +0000

    using cypress with ts

---
+++ cypress/integration/demo.spec.ts
@@ -0,0 +1,8 @@
+describe('Demo Test', () => {
+  it('Should login into an application', () => {
+    cy.visitLoginpage();
+    cy.fixture('loginData').then(({ username, password }) => {
+      cy.login(username, password);
+    });
+  });
+});
