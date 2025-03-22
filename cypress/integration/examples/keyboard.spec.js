4944750a18dfd0cb6af3f37b283aa96195bed248
NRotondaro <niksonrotondaro1@gmail.com>
2025-03-22 12:28:20 -0300

    chore: remove file to create new branch

--- cypress/integration/examples/keyboard.spec.js
+++
@@ -1,6 +0,0 @@
-describe('keyboard press simulation', () => {
-	it('should submit serachbox with pressing enter', () => {
-		cy.visit('http://zero.webappsecurity.com/index.html');
-		cy.get('#searchTerm').type('just some text {enter}');
-	});
-});
